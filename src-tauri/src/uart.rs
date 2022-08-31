15f36312d0394303d182179e6113fc15caca3799
craig kaiser <craig@kaiserengineering.io>
2022-08-31 17:49:44 -0400

    add some frontend example components

--- src-tauri/src/uart.rs
+++ src-tauri/src/uart.rs
@@ -1,25 +1,26 @@
-use serde::Serialize;
+use std::fmt;
 
-#[cfg(not(test))]
+use serde::Serialize;
 use serialport::available_ports;
 
-#[cfg(test)]
-use serialport::{SerialPortInfo, SerialPortType};
-#[cfg(test)]
-fn available_ports() -> Result<Vec<SerialPortInfo>, String> {
-    Ok(vec![SerialPortInfo {
-        port_name: "TestPort".to_string(),
-        port_type: SerialPortType::Unknown,
-    }])
+#[derive(Debug, Serialize)]
+pub enum SerialError {
+    Error(String),
 }
 
-#[derive(Serialize)]
+impl fmt::Display for SerialError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "An Error Occurred, Please Try Again!") // user-facing output
+    }
+}
+
+#[derive(Serialize, Debug)]
 pub struct SerialPort {
     port_name: String,
 }
 
 #[tauri::command]
-pub fn find_available_ports() -> Result<Vec<SerialPort>, String> {
+pub fn find_available_ports() -> Result<Vec<SerialPort>, SerialError> {
     // Return vec of all ports found on device
     match available_ports() {
         Ok(ports) => {
@@ -33,23 +34,6 @@ pub fn find_available_ports() -> Result<Vec<SerialPort>, String> {
                 })
                 .collect())
         }
-        Err(error) => Err(error.to_string()),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    // this brings everything from parent's scope into this scope
-    #[test]
-    fn test_find_available_ports() {
-        let ports = find_available_ports().unwrap();
-        assert_eq!(ports.len(), 1, "Found one port!");
-
-        assert_eq!(
-            ports[0].port_name, "TestPort",
-            "Port name set correctly via SerialPort struct"
-        );
+        Err(error) => Err(SerialError::Error(error.to_string())),
     }
 }
