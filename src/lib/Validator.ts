46b92a8de3e3d6ccfd20d064c47dd2919791576e
craig kaiser <craig@kaiserengineering.io>
2022-10-21 13:48:59 -0400

    update validator input

--- src/lib/Validator.ts
+++ src/lib/Validator.ts
@@ -2,7 +2,7 @@ export function validate_config(config) {
 	if (config['configType'] == 'RPM') {
 		let res = true;
 
-		res = res && config['SHIFT'] > config['activation_point'];
+		res = config['SHIFT'] > config['ACT'];
 
 		return { is_valid: res, error: 'Shift Point must be greater than Activation Point' };
 	}
