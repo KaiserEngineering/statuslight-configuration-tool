8963effa009ec5e9a1bfdcba7ffdb52c749b44e6
craig kaiser <craig@kaiserengineering.io>
2022-10-27 19:22:59 -0400

    make it nicer

--- src/routes/update/+page.svelte
+++ src/routes/update/+page.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { success, error } from '$lib/Toasts';
+	import { session } from '$lib/Store';
 
 	let changelog = '';
 	let hex = '';
 
 	async function checkForNewVersion() {
-		console.log('Going to update firmware');
+		$session.loading = true;
 		invoke('get_latest_firmware')
 			.then((res) => {
 				changelog = res.changelog;
 				hex = res.hex;
 			})
-			.catch((e) => error(e));
+			.catch((e) => error(e))
+			.finally(() => ($session.loading = false));
+	}
+
+	async function write_firmware() {
+		$session.loading = true;
+		if (hex == '') {
+			error('No firmware HEX content found, not doing anything');
+			return;
+		}
+		await invoke('write', { content: hex })
+			.then((res) => {
+				success(res);
+			})
+			.catch((err) => {
+				// Backend sends back SerialError object
+				error(err.message);
+			})
+			.finally(() => ($session.loading = false));
 	}
 </script>
 
@@ -26,5 +45,13 @@
 		on:click={checkForNewVersion}
 	>
 		Check for new version
+	</button>
+
+	<button
+		type="button"
+		class="dark:bg-slate-400 bg-gray-300 p-2 rounded m-2"
+		on:click={write_firmware}
+	>
+		Update firmware
 	</button>
 </div>
