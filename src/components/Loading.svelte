bbe841cd7c34403b45bb0da18572b644839ba1bf
craig kaiser <craig@kaiserengineering.io>
2022-10-27 20:22:38 -0400

    remove background from loading spinner

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -3,11 +3,11 @@
 </script>
 
 {#if $session.loading}
-	<div class="fixed h-full w-full flex justify-center bg-opacity-50 bg-gray-700">
+	<div class="fixed w-full flex justify-center">
 		<div role="status" class="top-40 absolute">
 			<svg
 				aria-hidden="true"
-				class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+				class="mr-2 w-8 h-8 text-gray-200 animate-spin fill-blue-600"
 				viewBox="0 0 100 101"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
