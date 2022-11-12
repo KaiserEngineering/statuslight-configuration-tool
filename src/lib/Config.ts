2615d95638b914c1f9a8f9a90edc9dad30829434
craig kaiser <craig@kaiserengineering.io>
2022-11-12 09:35:53 -0500

    update config.ts to be inline with firmware

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -8,7 +8,7 @@ export const ShiftLightConfigs = {
 			info: 'RPM value to flash shift light at.'
 		},
 		'Acquisition Mode': {
-			code: 'aquisition_mode',
+			code: 'ACQ',
 			type: [
 				{ label: 'OBDII_MODE', value: 'OBDII_MODE' },
 				{ label: 'CAN Bus Listen Mode', value: 'CAN Bus Listen Mode' },
@@ -21,12 +21,8 @@ export const ShiftLightConfigs = {
 			min: 0,
 			max: 16000
 		},
-		Illum: {
-			code: 'illum',
-			type: 'number'
-		},
 		'Animation Direction': {
-			code: 'ANIMATION_DIRECTION',
+			code: 'ANIM',
 			type: [
 				{ label: 'Left to Right', value: 'Left to Right' },
 				{ label: 'Right to left', value: 'Right to Left' },
@@ -35,7 +31,7 @@ export const ShiftLightConfigs = {
 			]
 		},
 		Color: {
-			code: 'color',
+			code: 'COLOR',
 			type: [
 				{ label: 'Blue Green Red', value: 0 },
 				{ label: 'Green Red Blue', value: 1 },
