c504069b99e303a6a1e9f2b83a5d69c0be016750
Craig Kaiser <craig@kaiserengineering.io>
2023-03-10 12:08:56 -0500

    make toast duration 2 sec

--- src/lib/Toasts.ts
+++ src/lib/Toasts.ts
@@ -7,20 +7,23 @@ export const success = (m) =>
 			'--toastColor': 'white',
 			'--toastBarBackground': 'olive'
 		},
+		duration: 2000,
 		pausable: true
 	});
 
-export const info = (m) =>
+export const info = (m: string) =>
 	toast.push(m, {
 		classes: ['bg-nord10'],
-		pausable: true
+		pausable: true,
+		duration: 2000
 	});
 
-export const error = (m) =>
+export const error = (m: string) =>
 	toast.push(m, {
 		theme: {
 			'--toastBackground': '#F56565',
 			'--toastBarBackground': '#C53030'
 		},
-		pausable: true
+		pausable: true,
+		duration: 2000
 	});
