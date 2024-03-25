c1a2b7c2c504baa087694371c4b7ac7d112f8244
Craig Kaiser <craig@kaiserengineering.io>
2024-03-25 16:04:44 -0400

    rm

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 pub fn main() {
-    #[cfg(target_os = "windows")]
-    unsafe {
-        winapi::um::shellscalingapi::SetProcessDpiAwareness(2);
-    }
-
     keshiftlight_configuration_tool_lib::run();
 }
