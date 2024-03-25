44c9e1e0dade3aeab595ccb04123c82e12054abc
Craig Kaiser <craig@kaiserengineering.io>
2024-03-25 16:02:32 -0400

    test scaling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 pub fn main() {
+    #[cfg(target_os = "windows")]
+    unsafe {
+        winapi::um::shellscalingapi::SetProcessDpiAwareness(2);
+    }
+
     keshiftlight_configuration_tool_lib::run();
 }
